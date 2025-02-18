1578fd324cf644ba287b47c7991a722bdf250c61
Clancy-dev <clancyro1789@gmail.com>
2025-02-18 15:12:52 +0300

    the

--- app/(Backend)/dashboard/users/page.tsx
+++ app/(Backend)/dashboard/users/page.tsx
@@ -4,6 +4,7 @@ export default function page() {
   return (
     <div>
       <h2>Dashboard users</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium sequi optio iusto? Quidem explicabo soluta iste voluptatibus assumenda quas veniam ad at libero, nisi, voluptate incidunt sed mollitia aut.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At similique qui, quam, reiciendis sit, eligendi amet doloremque veritatis porro earum fuga totam iure eius. Aperiam quae tenetur similique soluta officia?</p>
 
     </div>
