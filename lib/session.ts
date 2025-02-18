a4769b0f5fad4afb08e411bf4000606abe5715e5
Clancy-dev <clancyro1789@gmail.com>
2025-02-18 14:55:12 +0300

    the

--- lib/session.ts
+++ lib/session.ts
@@ -3,7 +3,7 @@ import { SignJWT, jwtVerify } from 'jose'
 import { User, UserRole } from '@prisma/client';
 import { cookies } from 'next/headers';
 
-export type SessionPayload={
+export type SessionPayloadProps={
     userId:string;
     role:UserRole;
     email:string;
@@ -11,26 +11,26 @@ export type SessionPayload={
     expiresAt:Date;
     
 }
-;
 
- 
-const secretKey = process.env.SECRET_KEY
-if (!secretKey) {
+//SECRET
+const mySecret = new TextEncoder().encode(process.env.SECRET_KEY)
+if (!mySecret) {
   throw new Error('SECRET_KEY is not defined in the environment variables.');
 }
-const encodedKey = new TextEncoder().encode(secretKey)
  
-export async function encrypt(payload: SessionPayload) {
+// ECRYPTING THE PAYLOAD DATA INTO A TOKEN
+export async function encrypt(payload: SessionPayloadProps) {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
     .setExpirationTime('7d')
-    .sign(encodedKey)
+    .sign(mySecret)
 }
  
+//DECRYPTING THE TOKEN TO GET BACK OUR PAYLOAD DATA
 export async function decrypt(session: string | undefined = '') {
   try {
-    const { payload } = await jwtVerify(session, encodedKey, {
+    const { payload } = await jwtVerify(session, mySecret, {
       algorithms: ['HS256'],
     })
     return payload
@@ -91,3 +91,165 @@ export async function deleteSession() {
   const cookieStore = await cookies()
   cookieStore.delete('session')
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'server-only'
+// import { SignJWT, jwtVerify } from 'jose'
+// import { User, UserRole } from '@prisma/client';
+// import { cookies } from 'next/headers';
+
+// export type SessionPayload={
+//     userId:string;
+//     role:UserRole;
+//     email:string;
+//     name:string;
+//     expiresAt:Date;
+    
+// }
+// ;
+
+ 
+// const secretKey = process.env.SECRET_KEY
+// if (!secretKey) {
+//   throw new Error('SECRET_KEY is not defined in the environment variables.');
+// }
+// const encodedKey = new TextEncoder().encode(secretKey)
+ 
+// export async function encrypt(payload: SessionPayload) {
+//   return new SignJWT(payload)
+//     .setProtectedHeader({ alg: 'HS256' })
+//     .setIssuedAt()
+//     .setExpirationTime('7d')
+//     .sign(encodedKey)
+// }
+ 
+// export async function decrypt(session: string | undefined = '') {
+//   try {
+//     const { payload } = await jwtVerify(session, encodedKey, {
+//       algorithms: ['HS256'],
+//     })
+//     return payload
+//   } catch (error) {
+//     console.log('Failed to verify session')
+//   }
+// }
+
+
+ 
+// export async function createSession(user:User) {
+//   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+//   const payLoadData = {
+//     userId:user.id,
+//     role:user.role,
+//     email:user.email,
+//     name:user.fullName,
+//     expiresAt:expiresAt,
+// }
+//   const session = await encrypt(payLoadData)
+//   const cookieStore = await cookies()
+ 
+//   cookieStore.set('session', session, {
+//     httpOnly: true,
+//     secure: true,
+//     expires: expiresAt,
+//     sameSite: 'lax',
+//     path: '/',
+//   })
+// }
+
+
+
+// //prevent someone logging in after use everytime, auto check and update the session time
+
+// export async function updateSession() {
+//   const session = (await cookies()).get('session')?.value
+//   const payload = await decrypt(session)
+ 
+//   if (!session || !payload) {
+//     return null
+//   }
+ 
+//   const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+ 
+//   const cookieStore = await cookies()
+//   cookieStore.set('session', session, {
+//     httpOnly: true,
+//     secure: true,
+//     expires: expires,
+//     sameSite: 'lax',
+//     path: '/',
+//   })
+// }
+
+
+// export async function deleteSession() {
+//   const cookieStore = await cookies()
+//   cookieStore.delete('session')
+// }
